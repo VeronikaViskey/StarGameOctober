0dbbb5171fbef5405ff96bbc4b5c449e7810fcfc
VeronikaViskey <veronika.viskey@yandex.ru>
2020-10-26 21:15:44 +0200

    дз 1

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import ru.geekbrains.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.height = 750;
-		config.width = 1000;
+		config.height = 650;
+		config.width = 900;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
